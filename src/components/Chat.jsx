75d185847d1d71c94f3f7a353b13df12589b4b2f
Elite6277 <hacklinux.93@gmail.com>
2023-08-07 04:39:38 +0500

    Finished_client1.1

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -9,7 +9,7 @@ import icon from "../images/emoji.svg";
 import styles from "../styles/Chat.module.css";
 import Messages from "./Messages";
 
-const socket = io.connect("https://online-chat-900l.onrender.com");
+const socket = io.connect("https://myonline-chat-serverv1-0.onrender.com");
 
 const Chat = () => {
   const { search } = useLocation();
